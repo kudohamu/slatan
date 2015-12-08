0112823f050f4bd0afef61da0fa5e6f57ae3066a
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-08 22:27:11 +0900

    Fix require files

--- lib/slatan.rb
+++ lib/slatan.rb
@@ -1,12 +1,17 @@
 require "slatan/version"
+require "slatan/spirit"
+require "slatan/heart"
+require "slatan/mouth"
+require "slatan/ear"
+require "utils/string_ex"
 
 module Slatan
   using StringEx
   Spirit.infuse
 
-  @heart = Slatan::Heart.new
-  @mouth = Slatan::Mouth.new @heart
-  @ear   = Slatan::Ear.new @mouth
+  @heart = Heart.new
+  @mouth = Mouth.new @heart
+  @ear   = Ear.new @mouth
 
   @heart.beat @ear
 end
