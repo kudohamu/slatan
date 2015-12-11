4324499e54fd5c74501cdae0a408d4c027c91729
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-11 09:27:23 +0900

    Create websocket system using Real Time Messaging API

--- lib/slatan/heart.rb
+++ lib/slatan/heart.rb
@@ -1,6 +1,42 @@
+require 'slatan/spirit'
+require 'slatan/buttocks'
+
+require 'faye/websocket'
+require 'eventmachine'
+require 'json'
+
 module Slatan
-  class SlackHeart < Heart
+  class Heart
+
+    @@slack_api_url = 'https://slack.com/api'
+
     def beat(ear)
+      req = Net::HTTP.post_form URI("#{@@slack_api_url}/rtm.start"), token: Spirit.see[:slack_token]
+      parsed_body = JSON.parse(req.body, symbolize_names: true)
+      ws_url = parsed_body[:url]
+      EM.run {
+        @ws = Faye::WebSocket::Client.new(ws_url)
+
+        @ws.on :open do |event|
+          p [:open]
+          Buttocks.debug('open')
+          @ws.send({type: 'hello'})
+        end
+
+        @ws.on :message do |event|
+          p [:message, event.data]
+          Buttocks.debug("message: #{event.data}")
+          data = JSON.parse(event.data, symbolize_names: true)
+          ear.hear(data)
+        end
+
+        @ws.on :close do |event|
+          p [:close, event.code, event.reason]
+          Buttocks.debug("close: #{event.code}(#{event.reason})")
+          @ws = nil
+          EM.stop
+        end
+      }
     end
   end
 end
