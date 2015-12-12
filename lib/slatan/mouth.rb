8087be8c23b6ca16e92c99db0d00b19d969bd4e1
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-12 18:52:21 +0900

    Change to return response

--- lib/slatan/mouth.rb
+++ lib/slatan/mouth.rb
@@ -1,11 +1,43 @@
 require 'slatan/spirit'
+require 'slatan/mouth/api'
+require 'slatan/mouth/auth'
+require 'slatan/mouth/channels'
 require 'slatan/mouth/chat'
+require 'slatan/mouth/emoji'
+require 'slatan/mouth/files'
+require 'slatan/mouth/groups'
+require 'slatan/mouth/im'
+require 'slatan/mouth/mpim'
+require 'slatan/mouth/pins'
+require 'slatan/mouth/reactions'
+require 'slatan/mouth/rtm'
+require 'slatan/mouth/stars'
+require 'slatan/mouth/team'
+require 'slatan/mouth/users'
+require 'slatan/buttocks'
 
 require 'net/http'
+require 'active_support/core_ext/object/try'
+require 'json'
 
 module Slatan
+  ## send and receive message to use Slack API
   module Mouth
+    include Api
+    include Auth
+    include Channels
     include Chat
+    include Emoji
+    include Files
+    include Groups
+    include Im
+    include Mpim
+    include Pins
+    include Reactions
+    include Rtm
+    include Stars
+    include Team
+    include Users
 
     class << self
       def send(category, method, msg)
@@ -18,16 +50,22 @@ module Slatan
         }.merge(msg)
 
         uri = URI.parse("#{base_url}/#{category}.#{method}?#{URI.encode_www_form(msg)}")
+
+        Buttocks.debug("sent request: #{uri.request_uri}")
+
         request = Net::HTTP::Post.new(uri.request_uri, {
           'Content-Type' =>'application/json'
         })
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        http.start do |h|
-          response = h.request(request)
+        response = http.start do |h|
+          h.request(request)
         end
+
+        body = JSON.parse(response.try(:body) || '{"ok":false, "error": " request body is empty."}', symbolize_names: true)
+        Buttocks.debug("received response: #{body}")
+        body
       end
     end
   end
