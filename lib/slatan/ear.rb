7b020c018a8f095802e5e75a7949367600e73e30
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-12 19:28:41 +0900

    Add condtion option to publish 'message' receive event

--- lib/slatan/ear.rb
+++ lib/slatan/ear.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/object/blank'
+
 module Slatan
   ## class to Event Dispatching for concerns
   module Ear
@@ -5,14 +7,19 @@ module Slatan
 
     class << self
       ## register subscriber
-      def register(concern)
-        @concerns << concern
+      # @param concern subscriber
+      # @param options option
+      #          cond: <Kernel.#lambda> call hear method of concern if cond.call(msg) is true
+      def register(concern, options={})
+        @concerns << [concern, options[:cond]]
       end
 
       ## publish to subscribers
       def hear(msg)
-        @concerns.each do |concern|
-          concern.hear(msg)
+        @concerns.each do |concern, cond|
+          if cond.blank? || cond.call(msg)
+            concern.hear(msg)
+          end
         end
       end
     end
