36431e2280c974a6badc2be7b690303b76372677
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-13 16:00:21 +0900

    Add daemonize mode to Slatan

--- lib/slatan.rb
+++ lib/slatan.rb
@@ -5,17 +5,37 @@ require "slatan/ear"
 require "slatan/buttocks"
 
 module Slatan
-
   class << self
-    def run
+    ## running slatan
+    # @param options
+    #         daemonize: running on daemon mode(default false)
+    def run(options = {})
+      {
+        daemonize: false
+      }.merge(options)
+
       Buttocks.init
 
       @heart = Heart.new
 
       begin
+        daemonize if options[:daemonize]
         @heart.beat
       rescue => e
         Buttocks.fatal "#{e.backtrace.first}: #{e.message} (#{e.class})"
+      end
+    end
+
+    def daemonize
+      begin
+        Process.daemon
+
+        File.open(Spirit.pid_file_path, 'w') do |f|
+          f << Process.pid
+        end
+      rescue => e
+        Buttocks.fatal "failed to daemonize slatan.(#{e.message})"
+        exit
       end
     end
   end
