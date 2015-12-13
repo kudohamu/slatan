78123f75a28129be251f201a143d2841792e736f
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-13 16:46:44 +0900

    Add some method Affiliation::Channels module

--- lib/slatan/affiliation/channels.rb
+++ lib/slatan/affiliation/channels.rb
@@ -11,6 +11,25 @@ module Slatan
         def set(channels)
           @channels = channels
         end
+
+        ## return id of channel name
+        # @param name: channel name
+        def get_id(name)
+          @channels.each do |channel|
+            return channel[:id] if channel[:name] == name
+          end
+          nil
+        end
+
+        ## return name of channel id
+        # @param id: channel id
+        def get_name(id)
+          @channels.each do |channel|
+            return channel[:name] if channel[:id] == id
+          end
+          nil
+        end
+
       end
     end
   end
