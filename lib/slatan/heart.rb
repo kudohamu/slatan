4e3c0b08ed52649e7511c6077d45f0fae48b93d1
kudohamu <kudohamu91086@yahoo.co.jp>
2015-12-13 19:48:31 +0900

    Add check system to prevent message loop

--- lib/slatan/heart.rb
+++ lib/slatan/heart.rb
@@ -22,13 +22,12 @@ module Slatan
 
         @ws.on :open do |event|
           Buttocks.info('open')
-          @ws.send({type: 'hello'})
         end
 
         @ws.on :message do |event|
           Buttocks.debug("message: #{event.data}")
           data = JSON.parse(event.data, symbolize_names: true)
-          Ear.hear(data)
+          Ear.hear(data) if enable_dispatch data
         end
 
         @ws.on :close do |event|
@@ -39,12 +38,51 @@ module Slatan
       }
     end
 
+    private
+
     def set_affiliation(affiliation)
       Affiliation::Self.set affiliation[:self]
       Affiliation::Team.set affiliation[:team]
       Affiliation::SubTeams.set affiliation[:sub_teams]
       Affiliation::Channels.set affiliation[:channels]
       Affiliation::Users.set affiliation[:users]
+    end
+
+    # check whether data enable dispatching
+    # @param [hash] received data
+    def enable_dispatch(data)
+      if Spirit.ignore_bot_reply && is_bot_reply?(data)
+        return false
+      end
+
+      if Spirit.ignore_self_message && is_self_message?(data)
+        return false
+      end
+
+      true
+    end
+
+    # check whether message is bot retry
+    # @return [bool]
+    def is_bot_reply?(data)
+      if data[:type] != 'message'
+        return false
+      end
+
+      if data[:text] =~ /^\s*<@#{Affiliation::Self.get[:id]}>:?.*$/ && Affiliation::Users.is_bot?(data[:user])
+        return true
+      end
+
+      false
+    end
+
+    # check whether self message
+    # @return [bool]
+    def is_self_message?(data)
+      if data[:type] == 'message' && data[:user] == Affiliation::Self.get[:id]
+        return true
+      end
+      false
     end
   end
 end
