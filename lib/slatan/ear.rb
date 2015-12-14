1a03f1f59068227254c6c95da5d661581cf8e416
kudohamu <yugo_tanaka@dwango.co.jp>
2015-12-14 18:13:23 +0900

    Change arg from Karnel.lambda to block of Slatan::Ear.register

--- lib/slatan/ear.rb
+++ lib/slatan/ear.rb
@@ -8,16 +8,15 @@ module Slatan
     class << self
       ## register subscriber
       # @param concern subscriber
-      # @param options option
-      #          cond: <Kernel.#lambda> call hear method of concern if cond.call(msg) is true
-      def register(concern, options={})
-        @concerns << [concern, options[:cond]]
+      # @param block<option> condition for dispatching event
+      def register(concern, &block)
+        @concerns << [concern, block]
       end
 
       ## publish to subscribers
       def hear(msg)
-        @concerns.each do |concern, cond|
-          if cond.blank? || cond.call(msg)
+        @concerns.each do |concern, block|
+          if block.blank? || block.call(msg)
             concern.hear(msg)
           end
         end
